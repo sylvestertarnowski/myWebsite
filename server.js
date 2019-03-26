e9a9e44d2b4b5efb19b879f34edfcdece97ce420
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-03-26 05:15:39 +0100

    Add body parser, database connect confirmation/error

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-import WordsModel from "./src/models/words";
+const bodyParser = require('body-parser');
+// import WordsModel from "./src/models/words";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,15 +9,21 @@ const port = process.env.PORT || 5000;
 //database connect
 const mongoDB = 'mongodb://localhost:27017/myWebsite'
 mongoose.set('useCreateIndex', true);
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.connect(mongoDB, {useNewUrlParser: true})
+        .then(() => {console.log('Database is connected')},
+        err => {console.log('Cannot connect to the database' + err)}
+);
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/save_words', (req, res) => {
-
+    
 });
 
 app.get('/express_backend', (req, res) => {
     res.send({express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
