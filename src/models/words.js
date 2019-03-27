c24662b604263adb7e70a1c02367b351069b00a5
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-03-27 05:37:31 +0100

    Change schema model for words

--- src/models/words.js
+++ src/models/words.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-const listSchema = new mongoose.Schema({
-    word: String,
-    translation: String
-});
-
 const wordsSchema = new mongoose.Schema({
     name: {
         type: String,
         unique: true
     },
     language: String,
-    list: [[listSchema]]
+    list: [
+        {
+            word: String,
+            translation: String
+        }
+    ]
 });
 
 module.exports = mongoose.model('Words', wordsSchema);
