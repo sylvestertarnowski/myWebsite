1858ea053dea1a3fb106c43e65e7597f45a03adb
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-03-30 10:21:25 +0100

    Setup of website schema, building paths

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-// import WordsModel from "./src/models/words";
-
-const app = express();
-const port = process.env.PORT || 5000;
-
+const express     = require('express');
+const mongoose    = require('mongoose');
+const bodyParser  = require('body-parser');
+const path        = require('path');
 const WordsRouter = require('./WordsRouter');
+const app         = express();
+const port        = process.env.PORT || 5000;
 
-//database connect
+// < --- database connect --- >
 const mongoDB = 'mongodb://localhost:27017/myWebsite'
 mongoose.set('useCreateIndex', true);
 mongoose.connect(mongoDB, {useNewUrlParser: true})
@@ -16,14 +14,21 @@ mongoose.connect(mongoDB, {useNewUrlParser: true})
         err => {console.log('Cannot connect to the database' + err)}
 );
 
+// < --- initial setup --- >
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+// < --- routes --- >
 app.use('/words', WordsRouter);
 
 app.get('/express_backend', (req, res) => {
     res.send({express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
