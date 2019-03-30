8012510471fd8da20b45bc571824dfbec007ed97
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2019-03-30 10:38:07 +0100

    public files are now served from public directory

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 // < --- routes --- >
 app.use('/words', WordsRouter);
